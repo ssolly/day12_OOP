2358225b4b1ad7bea60a7d280a3b3cb22e89e640
Choi.SY <sollly0829@gmail.com>
2021-08-11 12:39:44 +0900

    Quiz01 window7 프로그램 만들기

--- src/day12_OOP/Quiz01_Win7_Main.java
+++ src/day12_OOP/Quiz01_Win7_Main.java
@@ -1,5 +1,40 @@
 package day12_OOP;
 
+import java.util.HashMap;
+
 public class Quiz01_Win7_Main {
 
+	public static void main(String[] args) {
+		
+		Quiz01_method qm = new Quiz01_method();
+		
+		while(true) {
+			System.out.println("===== 환영합니다 =====");
+			System.out.println("1. 기능");
+			System.out.println("2. off");
+			int choice = qm.input();
+			
+			switch(choice) {
+			case 1 :
+				System.out.println("사용 가능 기능");
+				System.out.println("1 : 계산기");
+				System.out.println("2 : 메모장");
+				String str = qm.useInput();
+				if (str.equals("계산기")) {
+					qm.clac();
+				} else if (str.equals("메모장")) {
+					qm.notepad();
+				} else {
+					System.out.println("잘못된 입력입니다");
+				}
+				break;
+				
+			case 2:
+				System.out.println("종료");
+				return;
+			}
+			
+		}
+
+	}
 }
--- src/day12_OOP/Quiz01_method.java
+++ src/day12_OOP/Quiz01_method.java
@@ -1,5 +1,47 @@
 package day12_OOP;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Quiz01_method {
 
+	public int input() {
+		Scanner sc = new Scanner(System.in);
+		int input;
+		System.out.print("입력>>> ");
+		input = sc.nextInt();
+		return input;
+	}
+	
+	public String useInput() {
+		Scanner sc = new Scanner(System.in);
+		String input;
+		System.out.print("사용할 기능 입력>>> ");
+		input = sc.next();
+		return input;
+	}
+	
+	public void clac() {
+		System.out.println("calc.exe 동작 합니다");
+		ProcessBuilder pro = new ProcessBuilder("calc");
+		try {
+			pro.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void notepad() {
+		System.out.println("notepad.exe 동작 합니다");
+		ProcessBuilder pro = new ProcessBuilder("notepad");
+		try {
+			pro.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
